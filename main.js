e72b036a5540e64192eb3c933b0eba0882689845
AdminTroller <lauedmondyc@gmail.com>
2023-03-18 19:50:01 +0800

    fixed crackly credits song

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ var gunAmmo = [10, 25, 45, 8, 2];
 var playerAmmo = [gunAmmo[0], gunAmmo[1], gunAmmo[2], gunAmmo[3], gunAmmo[4]];
 var gunReload = [40, 60, 70, 60, 60];
 var playerReload = [gunReload[0], gunReload[1], gunReload[2], gunReload[3], gunReload[4]];
-var gunDamage = [1, 0.85, 0.5, 0.75, 5];
+var gunDamage = [1, 50, 0.5, 0.75, 5];
 var startupCooldown = 30;
 
 var coins = 0;
@@ -222,7 +222,7 @@ function preload() { // Load sprites
     MUSIC_BOSS = loadSound(PATH + 'Music/boss2.mp3');
     MUSIC_BOSS.setVolume(volume/100);
     MUSIC_CREDITS = loadSound(PATH + 'Music/credits_final.mp3');
-    MUSIC_CREDITS.setVolume(volume/70);
+    MUSIC_CREDITS.setVolume(volume/60);
 
     SFX_DEATH = loadSound(PATH + 'SFX/death.mp3');
     SFX_HIT = loadSound(PATH + 'SFX/hit.mp3');
@@ -360,7 +360,7 @@ function menu() {
     text('Programming - Edmond', 512, 480);
     text('Music - Edmond', 512, 500);
     text('Sprites - Isaac, mi_gusta', 512, 520);
-    text('Playtesting - Aaden, Isaac, Jovan', 512, 540);
+    text('Playtesting - Aaden, Isaac, Jovan, Leon, Chris, Addison', 512, 540);
 
     if (mouseIsPressed && mouseX >= 512 - 170 && mouseX <= 512 + 170 && mouseY >= 386 - 20 && mouseY <= 386 + 20) volumePressed = true;
     if (!mouseIsPressed) volumePressed = false;
@@ -1145,7 +1145,7 @@ function boss() {
             }
         }
         enemyBullets = [];
-        MUSIC_CREDITS.setVolume(volume/100);
+        MUSIC_CREDITS.setVolume(volume/60);
         MUSIC_CREDITS.play();
         state = "credits";
     }
